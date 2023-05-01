1638e12791f770bd431d1cf0c6a1e6c23d8b7dee
emilylutzker <90513038+emilylutzker@users.noreply.github.com>
2023-05-01 09:19:38 -0400

    added a possum

--- CS_TheWorld_Part3/GameMechanics/WorldBuilder.cs
+++ CS_TheWorld_Part3/GameMechanics/WorldBuilder.cs
@@ -26,6 +26,14 @@ public static partial class Program
             Name = "This Place",
             Description = "A barren plane with an ambient temperature around 22C and moderate humidity."
         };
+        
+        var possum  = StandardCreatures.Marsupial;
+
+        possum.Stats.Death += (sender, args) =>
+        {
+            OnCreatureDeath("possum", possum, "Himalayan Soup base is ready");
+        };
+        start.AddCreature("possum", possum);
 
         // Add an item directly into the area.
         // by creating the item directly inside this statement,
